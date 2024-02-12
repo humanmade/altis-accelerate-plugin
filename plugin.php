4271ac1f3073dba4ef9dc881624720e771765fa6
GitHub Action <builds@github.com>
2024-02-12 11:38:57 +0000

    Update from Altis Accelerate

--- plugin.php
+++ plugin.php
@@ -4,7 +4,7 @@
  * Description: Go beyond content with Altis Accelerate, the best way to personalize content in WordPress.
  * Author: Altis DXP
  * Author URI: https://www.altis-dxp.com/
- * Version: pull-request-public-mirror
+ * Version: 3.1.9
  * Text Domain: altis-accelerate
  * Update URI: https://wordpress.org/plugins/altis-accelerate/
  */
@@ -13,7 +13,7 @@ namespace Altis\Accelerate;
 
 const PLUGIN_FILE = __FILE__;
 const PLUGIN_DIR = __DIR__;
-const VERSION = 'pull-request-public-mirror';
+const VERSION = '3.1.9';
 
 if ( ! defined( 'ABSPATH' ) ) {
 	return;
